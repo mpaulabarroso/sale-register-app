0034ff4cebe130ea38676ef5d8ab7c9ed1e0e184
mpaulabarroso <mariapaulab90@gmail.com>
2024-04-11 19:46:07 -0300

    feat: add settings screen to navigation

--- src/navigation/DrawerNav.tsx
+++ src/navigation/DrawerNav.tsx
@@ -1,6 +1,7 @@
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { SaleOrder } from '@screens/sale-order/SaleOrder'
 import { SalesHistory } from '@screens/sales-history/SalesHistory'
+import { Settings } from '@screens/settings/Settings'
 
 const Drawer = createDrawerNavigator()
 
@@ -9,6 +10,7 @@ export function DrawerNav() {
         <Drawer.Navigator>
             <Drawer.Screen name="SaleOrder" component={SaleOrder} />
             <Drawer.Screen name="SalesHistory" component={SalesHistory} />
+            <Drawer.Screen name="Settings" component={Settings} />
         </Drawer.Navigator>
     )
 }
