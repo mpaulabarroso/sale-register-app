6c61940484007f71b226615b760b24ee41ac2607
mpaulabarroso <mariapaulab90@gmail.com>
2024-04-11 19:45:25 -0300

    feat: add settings screen

---
+++ src/screens/settings/Settings.tsx
@@ -0,0 +1,11 @@
+import { ContentView } from '@componets/ContentView'
+import { Text } from 'react-native'
+
+
+export function Settings() {
+    return (
+        <ContentView>            
+            <Text>esto es la configuracion</Text>
+        </ContentView>
+    )
+}
