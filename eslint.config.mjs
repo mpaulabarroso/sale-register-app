dc9d4cb3b2ea9cd447e38665cc9b24883cb38a3b
mpaulabarroso <mariapaulab90@gmail.com>
2024-04-10 21:46:59 -0300

    chore: initial config

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        rules: {
+            quotes: ['error', 'single'],
+            semi: ['error', 'never'],
+            'no-console': 'error',
+            'indent': ['error', 4]
+        }
+    }
+)
