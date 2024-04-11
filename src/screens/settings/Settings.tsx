6728cd9517482fc0cb8a8285ecac5dfcb7f93f1f
mpaulabarroso <mariapaulab90@gmail.com>
2024-04-11 19:49:06 -0300

    feat: add add payment component

--- src/screens/settings/Settings.tsx
+++ src/screens/settings/Settings.tsx
@@ -1,12 +1,14 @@
 import { ContentView } from '@componets/ContentView'
 import { Text } from 'react-native'
 import { AddCategories } from './components/AddCategories'
+import { AddPayment } from './components/AddPayment'
 
 
 export function Settings() {
     return (
         <ContentView>
             <AddCategories />
+            <AddPayment />
             <Text>esto es la configuracion</Text>
         </ContentView>
     )
