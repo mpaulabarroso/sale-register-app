d91be550c8f91897f58ae9c824857c1ed8dc180e
mpaulabarroso <mariapaulab90@gmail.com>
2024-04-17 00:57:52 -0300

    refactor: add sync sales styles

--- src/screens/settings/components/SyncSales.tsx
+++ src/screens/settings/components/SyncSales.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@componets/Button'
 import { Separator } from '@componets/Separator'
-import { styles } from '../utils/settingsStyles'
-import { View } from 'react-native'
+import { theme } from '@utils/theme'
+import { StyleSheet, View } from 'react-native'
 
 export function SyncSales() {
     return (
@@ -16,3 +16,28 @@ export function SyncSales() {
         </View>
     )
 }
+
+export const styles = StyleSheet.create({
+    buttonCont: {
+        height: 30,
+        paddingHorizontal: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        backgroundColor: theme.colors.primary,
+    },
+    buttonText: {
+        color: theme.colors.txtSecondary,
+        fontSize: 14,
+        fontStyle: 'normal',
+        fontWeight: '500',
+        lineHeight: 18,
+    },
+    contRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingVertical: 4,
+        paddingHorizontal: 8,
+    },
+})
