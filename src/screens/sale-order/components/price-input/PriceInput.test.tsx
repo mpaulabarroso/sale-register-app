8295581706070fe3593b8ad72074991a7f132199
mpaulabarroso <mariapaulab90@gmail.com>
2024-04-23 06:13:27 +0000

    feat: add price input tests

---
+++ src/screens/sale-order/components/price-input/PriceInput.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '@testing-library/jest-native/extend-expect'
+import { fireEvent, render } from '@testing-library/react-native'
+import { PriceInput } from './PriceInput'
+
+describe('price input', () => {
+    const mockItem = {
+        category: 'Verduleria',
+        price: ''
+    }
+
+    //renderiza correctamente
+    test('renders correctly', () => {
+        const { getByPlaceholderText } = render(<PriceInput item={mockItem} />)
+        const inputField = getByPlaceholderText('111.11')
+
+        expect(inputField).toBeDefined()
+    })
+
+    //cambia el precio del item
+    test('update item price when text is entered into TextInput', () => {
+        const { getByPlaceholderText } = render(<PriceInput item={mockItem} />)
+        const inputField = getByPlaceholderText('111.11')
+
+        fireEvent.changeText(inputField, '234')
+
+        expect(mockItem.price).toBe('234')
+
+    })
+})
